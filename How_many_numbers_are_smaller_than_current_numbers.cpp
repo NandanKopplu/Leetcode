c230d432d291a7e3939ab33b57493958ccfe78ae
nandan_kopplu <85996213+NandanKopplu@users.noreply.github.com>
2022-08-06 23:44:23 +0530

    Create How_many_numbers_are_smaller_than_current_numbers.cpp

---
+++ How_many_numbers_are_smaller_than_current_numbers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        int count=0;
+        vector<int> v;
+        for(int i=0 ; i<nums.size() ; i++) 
+        {
+            for(int j=0 ; j<nums.size() ; j++)
+            {
+                if(nums[j] < nums[i])
+                {
+                    count++;
+                }
+            }
+            v.push_back(count);
+            count=0;
+        }
+        return v;
+    }
+};f
