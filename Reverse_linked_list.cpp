d5fcb16b167074eac1601afc87e20bf2cde9400d
nandan <nandankopplu2002@gmail.com>
2022-09-02 23:40:09 +0530

    Reverse linked list solution added

---
+++ Reverse_linked_list.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *temp, *cur;
+        if (head == NULL)
+        {
+            return head;
+        }
+        temp = NULL;
+        while (head != NULL)
+        {
+            cur = head;
+            head = head->next;
+            cur->next = temp;
+            temp = cur;
+        }
+        return temp;
+    }
+};
