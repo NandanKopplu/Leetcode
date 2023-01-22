c7e3cd144a8d5dd26f126fd4759db297442fec9e
Nandan_Kopplu <85996213+NandanKopplu@users.noreply.github.com>
2023-01-22 22:08:40 +0530

    Added solution - LeetHub

---
+++ Sum of Beauty of All Substrings - GFG/sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int beautySum(string s) {
+        int n=s.length();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            unordered_map<char,int>mp;
+            for(int j=i;j<n;j++){
+                mp[s[j]]++;
+                int mn=INT_MAX;
+                int mx=INT_MIN;
+                for(auto it:mp){
+                    if(it.second>mx)
+                        mx=it.second;
+                    if(it.second<mn)
+                        mn=it.second;
+                }
+                sum+=(mx-mn);
+            }
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution obj;
+        cout << obj.beautySum(s) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
